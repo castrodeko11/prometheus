af0a5d7d35c21a9f21e098159bea405b888c3647
André Oliveira <castrodeko11@hotmail.com>
2022-01-27 09:27:16 -0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,54 @@
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Histogram;
+import io.prometheus.client.Summary;
+import io.prometheus.client.exporter.HTTPServer;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static final Counter requests = Counter.build()
+            .name("requests_total").help("Total requests.").register();
+
+    private static double rand(double min, double max) {
+        return min + (Math.random() * (max - min));
+    }
+
+    public static void main(String[] args) {
+        Counter counter = Counter.build().namespace("java").name("my_counter").help("This is my counter").register();
+        Gauge gauge = Gauge.build().namespace("java").name("my_gauge").help("This is my gauge").register();
+        Histogram histogram = Histogram.build().namespace("java").name("my_histogram").help("This is my histogram").register();
+        Summary summary = Summary.build().namespace("java").name("my_summary").help("This is my summary").register();
+
+
+        final Counter failedRequests = Counter.build()
+                .name("requests_failed_total").help("Total failed requests.").register();
+
+        Thread bgThread = new Thread(() -> {
+
+            while (true) {
+                try {
+                    counter.inc(rand(0, 5));
+                    gauge.set(rand(-5, 10));
+                    histogram.observe(rand(0, 5));
+                    summary.observe(rand(0, 5));
+
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        bgThread.start();
+
+        try {
+
+            HTTPServer server = new HTTPServer(8080);
+
+        } catch (IOException e) {
+            failedRequests.inc();
+
+        }
+    }
+}
